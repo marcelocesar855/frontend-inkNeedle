c73da50f69e1f1affbbe40840391c7891a2b1dbd
marcelo cesar <marcelocesar855@gmail.com>
2019-11-07 02:22:47 -0300

    Agenda

--- src/components/EventCalendar.js
+++ src/components/EventCalendar.js
@@ -2,18 +2,32 @@ import React, {Component} from 'react';
 import '../styles/General.css'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import '../styles/Agenda.css';
+import { nextTick } from 'q';
 
     export default class EventCalendar extends Component {
 
         render () {
             return(
                 <FullCalendar defaultView="dayGridMonth"
-                plugins={dayGridPlugin }
-                weekends={false}
+                plugins={ [dayGridPlugin] } buttonText={{today : 'Hoje'}}
+                header={{center : 'title', left : 'myCustomButton', right : 'today, ,prev,next'}}
+                weekends={false} locale="pt-BR" weekends='true' fixedWeekCount={false}
+                customButtons = {{
+                    myCustomButton: {
+                      text: 'Agendar sessão',
+                      click: function() {
+                        alert('clicked the custom button!');
+                      }
+                    }
+                  }}
                 events={[
-                  { title: 'event 1', date: '2019-04-01' },
-                  { title: 'event 2', date: '2019-04-02' }
-                ]} />
+                    { title: '19:30h - Rodrigo Fonseca', date: '2019-11-01' },
+                    { title: '15:30h - Jessica Valadão', date: '2019-11-01' },
+                    { title: '19:30h - Rodrigo Fonseca', date: '2019-11-01' },
+                    { title: '15:30h - Jessica Valadão', date: '2019-11-01' }
+                  ]} 
+                />
             )
         }
 
--- src/pages/Agenda.js
+++ src/pages/Agenda.js
@@ -6,25 +6,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import EventCalendar from '../components/EventCalendar';
 
 export default class Agenda extends Component {
-
-    
-
     handleInputChange =  e => {
 
     };
 
   render() {
       return(
-            <div className="wrapper">
+            <div className="wrapper wrapper-logado">
                 <ul className="navbar navbar-fixed-top justify-content-end">
                     <li><a className="text-white" onClick={() => {this.props.history.push('/');}}>Minha Conta</a></li>
                     <li><a className="text-white" onClick={() => {this.props.history.push('/');}}>Ajuda</a></li>
                 </ul>
-                <div className="wrapper wrapper-logado">
                 <div className="container">
                 <div className="row">
                     <EventCalendar></EventCalendar>
-                </div>
                 </div>
                 </div>
                 <div className="footer-copyright text-center py-2 rodape">2019 - InkNeedle</div>
