11813547ee8d0715add191dd917748abc27f56c6
Marcelo César <33355145+marcelocesar855@users.noreply.github.com>
2019-11-26 17:34:57 -0300

    Add files via upload

---
+++ src/components/Notifications.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import {List, Media, Avatar} from "tabler-react";
+import '../styles/Notifications.css';
+import 'react-toastify/dist/ReactToastify.css';
+import '../styles/Login.css';
+import '../styles/General.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { getUser } from '../services/auth';
+import avatarDefault from './../images/avatar.png';
+
+export default class Notifications extends Component {
+
+  state = {
+    user: getUser(),
+    notifications : [{id : 1, nome : 'Marcelo César', content :'iis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', read : false},
+        {id : 2, nome : 'Marcelo César', content : ' ut fermentum massa justo sit amet risus.', read : false},
+        {id : 3, nome : 'Marcelo César', content : 'risque nisl consectetur et. Sed posuere consectetur est at lobortis.', read : true},
+        {id : 4, nome : 'Marcelo César', content : 'am porta sem malesuada magna mollis euismod. Donec sed odio dui.', read : true}],
+  }
+
+  getAvatar() {
+    const { user } = this.state;
+    return (!!user.avatarUrl ? user.avatarUrl : avatarDefault);
+}
+
+  render() {
+      return(
+        <div class = "notification">
+          <div class = "number"> </div>
+          <i class="fas fa-bell notBtn">
+            <div class = "box">
+              <div class = "display">
+                <div class = "nothing"> 
+                  <i class="fas fa-child stick"></i> 
+                  <div class = "cent">Você está atualizado em tudo!</div>
+                </div>
+                <div class = "cont">
+                {this.state.notifications.map(secao => (
+                <div class={'sec', secao.read ? '': 'new'} >
+                        <List.GroupItem className='sec-int'>
+                        <Media>
+                            <Avatar size="md" imageURL={this.getAvatar()}></Avatar>
+                            <Media.Body className="ml-3">
+                                <Media.Heading>
+                                    <h4>{secao.nome}</h4>
+                                </Media.Heading>
+                                <small>{secao.content.split('\n').map(function(item) {
+                                    return (
+                                        <span>
+                                        {item}
+                                        <br/>
+                                        </span>
+                                    )
+                                    })}</small>
+                            </Media.Body>
+                        </Media>
+                    </List.GroupItem>
+                    
+               </div>
+                    ))}
+                  </div>
+              </div>
+           </div>
+           </i>
+      </div>
+      );
+  }
+}
