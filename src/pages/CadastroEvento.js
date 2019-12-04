db5a9da25575697b98e885b0772867944dff2658
Antonio Leonardo Bezerra <abezerra@brasal.com.br>
2019-12-04 18:50:31 -0200

    Add estudio

--- src/pages/CadastroEvento.js
+++ src/pages/CadastroEvento.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import api from '../services/api';
 import '../styles/CadastroEstudio.css';
 import { Form } from "tabler-react";
 import '../styles/General.css';
@@ -13,6 +12,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { DateRangePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 import 'react-dates/initialize';
+
+import api from '../services/api';
+import Swal from 'sweetalert2'
 import moment from 'moment';
 
 export default class CadastroEvento extends Component {
@@ -20,18 +22,16 @@ export default class CadastroEvento extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            nome : '',
-            value : {
-                start : '00:00',
-                end : '23:59'
-            },
-            estudio : null,
+            studios: [],
+            studioId: null,
+            eventTypes: [],
+            eventTypeId: null,
+            name : '',            
             startDate : moment(),
             endDate : moment(),
-            precoEntrada : '',
             selectedFile : null,
             focusedInput : null,
-            descricao : '',
+            description : '',
             rows : 2,
         };
         this.featureRef = React.createRef();
@@ -39,36 +39,62 @@ export default class CadastroEvento extends Component {
         this.handleInputChangeDatas = this.handleInputChangeDatas.bind(this);
     }
 
-    handleInputChangeNome = e => { //possibilita a edição do texto no input
-        this.setState({nome : e.target.value});
+    componentDidMount() {
+        this.getStudios();        
+        this.getEventTypes();        
+    }
+
+    getStudios() {
+        api.get(`/studios`)
+        .then(res => {
+            const studios = res.data;
+            this.setState({ studios });
+        })
+    }
+
+    getEventTypes() {
+        api.get(`/event-types`)
+        .then(res => {
+            const eventTypes = res.data;
+            this.setState({ eventTypes });
+        })
+    }
+
+    handleInputChangeName = e => { //possibilita a edição do texto no input
+        this.setState({name : e.target.value});
+    };
+
+    handleInputChangeStudio = e => { //possibilita a edição do texto no input
+        this.setState({studioId : e.target.value});
+    };
+    handleInputChangeEventType = e => { //possibilita a edição do texto no input
+        this.setState({eventTypeId : e.target.value});
     };
 
     handleInputChangeEstudio = e => { //possibilita a edição do texto no input
-        this.setState({estudio : e.target.value});
+        this.setState({studioId : e.target.value});
     };
 
     handleInputChangeDatas ({ startDate, endDate }) { //possibilita a edição do texto no input
         this.setState({ startDate , endDate });
     };
 
-    handleInputChangeDescricao = e => { //possibilita a edição do texto no input
+    handleInputChangeDescritption = e => { //possibilita a edição do texto no input
         const lineHeight = 20;
         const previousRows = e.target.rows;
         e.target.rows = 1;
         const currentRows = ~~(e.target.scrollHeight / lineHeight);
+
         if (currentRows === previousRows) {
             e.target.rows = currentRows;
         }
+
         this.setState({ 
-            descricao : e.target.value,
+            description : e.target.value,
             rows : currentRows
         })
     };
-
-    handleInputChangePrecoEntrada = e => { //possibilita a edição do texto no input
-        this.setState({precoEntrada : e.target.value});
-    };
-
+    
     handleInputChangeDuracao(time) { //possibilita a edição do texto no input
         this.setState({value : time});
     };
@@ -91,53 +117,83 @@ export default class CadastroEvento extends Component {
 
     handleSubmit = async (e) => { //envia as informações a serem salvar para o backend
         e.preventDefault();
-        toast.configure();
-        var erro = '';
-        const nome = this.state.nome;
-        const estudio = this.state.estudio;
-        const inicio = this.state.startDate;
-        const fim = this.state.endDate;
-        const descricao = this.state.descricao;
-        var precoEntrada = null;
-        if (this.state.precoEntrada == '') {
-            precoEntrada = 0.0
-        }else {
-            precoEntrada = accounting.unformat(this.state.precoEntrada, ',');
-        }
-        const {value} = this.state.value;
-        const data = new FormData();
-        data.append('file', this.state.selectedFile);
-        if(nome !== ''){
-            if(estudio !== 0){
-                if(inicio !== null && fim !== null){
-                    if(value !== null){
-                        await api.post("events/", {nome,estudio,inicio,fim,descricao,precoEntrada,value,data}).then(response => {
-                            toast.success("Evento cadastrado com sucesso.",{
-                                position: "top-right",
-                                autoClose: 5000,
-                                hideProgressBar: false,
-                                closeOnClick: true,
-                                pauseOnHover: true
-                            })
-                            this.props.history.push('/perfil_tatuador');
-                        })
-                    }else{
-                        erro = "Informe o horário de realização do evento.";
-                        this.pushErrorMessage(erro)
+
+        const { name, studioId, eventTypeId, selectedFile, startDate, endDate } = this.state;
+
+        let description = this.state.description;
+
+        let erro = '';     
+ 
+        if(name !== ''){
+            if (!!studioId){
+                if (!!eventTypeId) {
+                    if (description !== '') {    
+                        description = `${description} | Data do evento ${startDate} - ${endDate}`                    
+                        if (!!startDate){
+                            await this.saveEvent();
+                        }else{
+                            erro = "Informe a data do evento";
+                        }
+                    } else {
+                        erro = "Informe a descrição sede do evento.";
                     }
-                }else{
-                    erro = "Informe as datas do evento.";
-                    this.pushErrorMessage(erro)
-                }
+                } else {
+                    erro = "Informe o tipo de evento.";
+                }               
+                
             }else{
                 erro = "Informe o estúdio sede do evento.";
-                this.pushErrorMessage(erro)
             }
         }else{
             erro = "Informe o nome do evento.";
-            this.pushErrorMessage(erro)
         }
+
+        if(!!erro) {
+            Swal.fire({
+                title: 'Validação!',
+                text: erro,
+                icon: 'danger',
+                showCancelButton: false,
+                confirmButtonColor: '#FF8C00',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Ok',
+            });
+        }        
     };
+
+    saveEvent() {
+        const { name, studioId, eventTypeId, selectedFile, description, startDate } = this.state;
+
+        let url = `/studio-store-event`;
+
+        const formData = new FormData();
+
+        formData.append('title', name);
+        formData.append('description', description);
+        formData.append('studioId', studioId);
+        formData.append('eventTypeId', eventTypeId);
+        formData.append('dateHour', startDate);
+        formData.append('file', selectedFile);
+
+        api({
+            method: 'post',
+            url,
+            data: formData,
+            headers: { 'Content-Type': 'multipart/form-data' }
+        })
+            .then((response) => {
+                Swal.fire(
+                    'Adicionado!',
+                    'Evento adicionada com sucesso',
+                    'success'
+                ).then((res) => {
+                    this.props.history.push(`/meu_estudio/${studioId}`);
+                });
+            })
+            .catch((response) => {
+                console.log(response);
+            });
+    }
 
     render() { //renderiza html
         return (
@@ -147,9 +203,9 @@ export default class CadastroEvento extends Component {
                     <div className="titulo mt-4">
                         <h1>Informe os dados abaixo para o cadastrar o evento a ser realizado</h1>
                     </div>
-                    <form className="formulario mb-5">
-                        <p>Nome:&nbsp;<input value={this.state.nome}
-                        onChange={this.handleInputChangeNome} placeholder="Nome do evento"></input></p>
+                    <form className="formulario mb-5" onSubmit={this.handleSubmit}>
+                        <p>Nome:&nbsp;<input value={this.state.name}
+                        onChange={this.handleInputChangeName} placeholder="Nome do evento"></input></p>
                             <p>Data:&nbsp;
                             <DateRangePicker
                                 startDate={this.state.startDate} // momentPropTypes.momentObj or null,
@@ -161,21 +217,24 @@ export default class CadastroEvento extends Component {
                                 focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
                                 onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
                             />
-                            </p>
-                        <p>Estúdio:&nbsp;<select value={this.state.bodyPiercing} onChange={this.handleInputChangeBodyPiercing}>
-                            <option value="0" disabled selected>Sede do evento</option>
+                        </p>
+                        <p>Estúdio:&nbsp;
+                        <select value={this.state.studioId} onChange={this.handleInputChangeStudio}>
+                            <option value="">Sede do evento</option>
+                            {this.state.studios.map(studio => {
+                                return (<option value={studio.id}>{studio.name}</option>);
+                            })}
                         </select></p>
-                        <p>Preço:&nbsp;<CurrencyFormat type="text" value={this.state.precoEntrada}
-                        onChange={this.handleInputChangePrecoEntrada} thousandSeparator="."
-                        decimalSeparator="," decimalScale="2" prefix="R$ " fixedDecimalScale="true" placeholder="Grátis? Deixe em branco"></CurrencyFormat></p>
-                        <div className="funcionamento">
-                            <p>Duração:&nbsp;De&nbsp;{this.state.value.start}&nbsp;às&nbsp;{this.state.value.end}</p>
-                            <TimeRange format={24} value={this.state.value} maxValue={"23:59"} minValue={"00:00"}
-                            onChange={this.handleInputChangeDuracao} step={15} name={"time_range"}></TimeRange>
-                        </div>
-                        <Form.Textarea rows={this.state.rows} id='desc' onChange={this.handleInputChangeDescricao}
+                        <p>Tipo de Evento:&nbsp;
+                        <select value={this.state.eventTypeId} onChange={this.handleInputChangeEventType}>
+                            <option value="">Sede do evento</option>
+                            {this.state.eventTypes.map(studio => {
+                                return (<option value={studio.id}>{studio.name}</option>);
+                            })}
+                        </select></p>               
+                        <Form.Textarea rows={this.state.rows} id='desc' onChange={this.handleInputChangeDescritption}
                             placeholder="Descrição do evento"
-                        value={this.state.descricao} className="post my-3"
+                        value={this.state.description} className="post my-3"
                         />
                         <div className="justify-content-center">
                             <div className="form-group files">
@@ -186,7 +245,7 @@ export default class CadastroEvento extends Component {
                                 onChange={this.handleInputChangeBanner}></input>
                             </div>
 	                    </div>
-                        <button onClick={this.handleSubmit}>Cadastrar evento</button>
+                        <button type="submit">Cadastrar evento</button>
                     </form>
                 </div>
                 <div className="footer-copyright text-center py-2 rodape">2019 - InkNeedle</div>
